584729efa1889fa18cc6b341ee9247dcf3728675
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-15 13:19:52 -0600

    creates getClass util, creates Image component, renders images in Peices page

--- src/pages/Pieces.jsx
+++ src/pages/Pieces.jsx
@@ -1,7 +1,16 @@
+import { useContext } from "react"
+import { Context } from "../components/ContextProvider"
+import Image from "../components/Image"
+import getClass from "../utils/getClass"
+
 export default function Pieces() {
+    const {allPhotos} = useContext(Context)
+    const images = allPhotos.map(i => (
+        <Image key={i.id} img={i.url} className={getClass(allPhotos.indexOf(i))} />
+    ))
     return (
         <main className="pieces">
-            <h1>Images go here</h1>
+            {images}
         </main>
     )
 }
