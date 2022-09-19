c67351dfaaa39dae2efc82f42c549c9af9e4ff69
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-19 17:41:28 -0600

    changes cart icon in header based on cart empty or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import {Link} from 'react-router-dom'
+import { useContext } from 'react'
+import { Context } from './ContextProvider'
 
 export default function Header() {
+    const {cartItems} = useContext(Context)
     return (
         <header>
             <Link to="/">
                 <h2>1oft</h2>
             </Link>
             <Link to="/cart">
-                <i className="ri-shopping-cart-2-fill ri-xl"></i>
-                <i className="ri-shopping-cart-2-line ri-xl"></i> {/**change based on if cart full or not */}
+                {cartItems.length ? 
+                    <i className="ri-shopping-cart-2-fill ri-xl"></i> :
+                    <i className="ri-shopping-cart-2-line ri-xl"></i>
+                }
             </Link>
         </header>
     )
