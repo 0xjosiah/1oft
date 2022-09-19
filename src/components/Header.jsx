6f822e8d9d436d114ef43a7a7f7d030913b86426
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-19 17:53:11 -0600

    refactor code to show cart icon in header based on cart empty or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,7 @@ export default function Header() {
                 <h2>1oft</h2>
             </Link>
             <Link to="/cart">
-                {cartItems.length ? 
-                    <i className="ri-shopping-cart-2-fill ri-xl"></i> :
-                    <i className="ri-shopping-cart-2-line ri-xl"></i>
-                }
+                <i className={`ri-shopping-cart-2-${cartItems.length ? "fill" : "line"} ri-xl`}></i>
             </Link>
         </header>
     )
