090216a405116bbac0267e67952eca53157c5cd5
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-20 16:25:01 -0600

    implements removeFromCart when on cart page, adds more ui to cart page

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,14 @@
 import { Context } from "./ContextProvider";
 import { useContext } from "react";
 
-export default function CartItem({img}) {
-    const {url, id} = img
+export default function CartItem({item}) {
+    const {url, id} = item
     const {removeFromCart} = useContext(Context)
     return (
-        <div className='cart-item' key={id}>
-            <img src={url} />
+        <div className='cart-item'>
+            <i onClick={() => removeFromCart(item)} className="ri-delete-bin-line"></i>
+            <img src={url} width="200px" />
+            <p>$5.99</p>
         </div>
     )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,13 +4,17 @@ import { Context } from "../components/ContextProvider"
 
 function Cart() {
     const {cartItems} = useContext(Context)
-    const cartImgs = cartItems.map(img => (
-        <CartItem img={img} />
+    const cartImgs = cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
     ))
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItems.length ? cartImgs : 'Your cart is empty'}
+            <p className="total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
         </main>
     )
 }
