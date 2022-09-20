d79469796c72d29e43618d408d4e990953e837db
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-19 18:01:38 -0600

    renders imgs in cart on Cart page, will need refactor

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,19 @@
-export default function Cart() {
+import { useContext } from "react"
+import { Context } from "../components/ContextProvider"
+
+function Cart() {
+    const {cartItems} = useContext(Context)
+    const cartImgs = cartItems.map(i => (
+        <div className='cart-item'>
+            <img src={i.url} />
+        </div>
+    ))
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartImgs}
         </main>
     )
 }
+
+export default Cart
