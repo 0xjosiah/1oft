4755609bb46e80453d1299fb9d75edcaf526c5c0
Josiah Webb <jj.webb520@gmail.com>
2023-03-27 19:47:28 -0600

    adjusts photo sizing fn

--- src/utils/getClass.js
+++ src/utils/getClass.js
@@ -1,4 +1,8 @@
 export default function getClass(i) {
+    // this is og for size variation
     if (i % 5 === 0) return 'big'
     if (i % 6 === 0) return 'wide'
+
+    // this is just the wide variant
+    // return 'wide'
 }
